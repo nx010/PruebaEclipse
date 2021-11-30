d765a6f853bbbf889d0a320d83ffb36b1ada8754
Antonio Martinez <antoniomartinez0791@gmail.com>
2021-11-30 13:49:52 +0100

    Versión Inicial

---
+++ src/com/test/Circle.java
@@ -0,0 +1,38 @@
+package com.test;
+
+public class Circle extends Figure {
+	
+	private double radius;
+	private static final double PI=3.1415;
+	
+	public Circle(double r, String name) {
+		
+		super(name);
+		
+		radius=r;
+	}
+	
+	public String toString() {
+		
+		return "Soy un círculo de radio " + radius;
+	}
+
+
+	public double calculateArea() {
+
+		return PI*radius*radius;
+	}
+
+
+	public double getPerimeter() {
+		
+		return 2*PI*radius;
+	}
+
+	@Override
+	public double calculatePerimeter() {
+		// TODO Apéndice de método generado automáticamente
+		return 0;
+	}
+
+}
---
+++ src/com/test/Figure.java
@@ -0,0 +1,22 @@
+package com.test;
+
+public abstract class Figure {
+	
+	public String name;
+	
+	public Figure (String n) {
+		
+		name=n;
+	}
+	
+	public String toString() {
+		
+		return name;
+	}
+	
+	public abstract double calculateArea();
+	
+	public abstract double calculatePerimeter();
+
+
+}
